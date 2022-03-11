b0dadd961dc3eb5b5e830c1cdeb4213c70c24fa1
goro-hyronjs <52033630+goro-hyronjs@users.noreply.github.com>
2022-03-11 12:53:47 +0800

    firstcommit

---
+++ src/views/MapVisualization.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="container">
+            <div style="width:25%;">
+                <el-input
+                placeholder="请输入内容"
+                prefix-icon="el-icon-search">
+                </el-input>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "mapVisualization",
+};
+</script>
+
+<style scoped>
+</style>
